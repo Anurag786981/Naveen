b024e7f080a6a56e73906e9727f2a9cb98f75fac
lenovo <lenovo@ANURAG>
2021-06-02 09:11:01 +0530

    firstCommitment

---
+++ gitDemo2/src/gitDemo2/Test1.java
@@ -0,0 +1,14 @@
+package gitDemo2;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		String hungryString = "yes";
+
+		if (hungryString == "yes")
+			System.out.println("eat Somthing");
+
+		else
+			System.out.println("do work");
+	}
+}
