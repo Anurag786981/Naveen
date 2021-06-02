a87b8e71d2a3a3e613763afd9ad77664290738ad
lenovo <lenovo@ANURAG>
2021-06-02 11:14:13 +0530

    Commiting test 2 to branch x

---
+++ gitDemo2/src/gitDemo2/Trest2.java
@@ -0,0 +1,16 @@
+
+public class Trest2 {
+
+	public static void main(String[] args) {
+		String hungryString = "yes";
+
+		if (hungryString == "yes")
+			System.out.println("eat Somthing");
+
+		else
+			System.out.println("do work");
+	}
+
+	}
+
+}
